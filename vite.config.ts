17eb22d2729fc08d127c234b9168d1f9005868f8
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-07-26 03:53:33 +0900

    :wrench: Update vite configuration file.
    viteの設定を開発しやすいように変更
    index.htmlをsrcに移動

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  root: "./src",
+  build: {
+    // root (= ./src) から見た相対パスで指定
+    outDir: "../public",
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  plugins: [react()],
+});
