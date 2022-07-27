8d18fca13a252d7061c54cd7cc1de1defa20f6e1
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-07-27 22:59:49 +0900

    :wrench: Update update .eslintrc.js
    eslintによるimportエラーを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,36 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  settings: {
+    // importのリゾルバー
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"],
+      },
+    },
+  },
   plugins: ["react", "@typescript-eslint"],
   rules: {
+    // Reactのimportを必ず必要としない
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
+
+    // jsx拡張子
     "react/jsx-filename-extension": [2, { extensions: [".ts", ".tsx"] }],
+
+    // 無関係なパッケージの使用を禁止
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+
+    // ファイル拡張子を一貫して使用
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    // single quoteでなくてもok
     quotes: ["off", "single"],
   },
 };
