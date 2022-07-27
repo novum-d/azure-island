3211ebc309712587fb6a59d790d31c1f6cb6a796
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-07-28 00:20:59 +0900

    :heavy_plus_sign: Add a vite-plugin-svgr dependency
    viteを使用してsvgをimportするための依存関係を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   root: "./src",
@@ -16,5 +17,5 @@ export default defineConfig({
       localsConvention: "camelCase",
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
