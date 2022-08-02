58c4cc63058ab3ac9fc9ab9ede65ba37cb37319a
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-08-03 08:58:42 +0900

    画面のリロード時に、themeがリセットされるバグを修正

---
+++ src/hooks/useLocalTheme.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import themes from "../theme/theme";
+import useLocalStorage from "./useLocalStorage";
+
+const key = "theme";
+
+const useLocalTheme = () => {
+  const [mode, storeMode] = useLocalStorage(
+    key,
+    themes.lightTheme.palette.mode
+  );
+
+  const theme = useMemo(() => {
+    const lightMode = mode === "light";
+    const switched = lightMode ? themes.lightTheme : themes.darkTheme;
+    return switched;
+  }, [mode]);
+
+  const switchTheme = () => storeMode(mode === "light" ? "dark" : "light");
+
+  return [theme, switchTheme] as const;
+};
+
+export default useLocalTheme;
